7531dfe1faedbb4cd3268dfc05bdc3d1b81d8b63
echavarriasam <echavarriasam@gmail.com>
2025-04-18 04:46:13 +0000

    wip

--- src/philosofer.rs
+++ src/philosofer.rs
@@ -5,3 +5,43 @@ pub struct Philosofer {
     pub left: Stick,
     pub right: Stick,
 }
+
+// Define methods within an impl block
+impl Philosofer {
+    // This is an associated function (often used as a constructor)
+    pub fn new(name: String, left: Stick, right: Stick) -> Philosofer {
+        Philosofer { name, left, right }
+    }
+
+    // retorna el nombre del filosofo como una referencia de String
+    pub fn get_name(&self) -> &String {
+        &self.name
+    }
+
+    // comer
+    pub fn eat(&mut self) {
+        // Logic for the philosopher eating
+        println!("{} está comiendo.", self.name);
+        // In a real scenario, you would need logic to acquire the sticks (locks)
+    }
+
+    // pensar,
+    pub fn think(&mut self) {
+        // Logic for the philosopher thinking
+        println!("{} está pensando.", self.name);
+        // In a real scenario, you would need logic to release the sticks (locks)
+    }
+
+    // intentar comer
+    pub fn try_to_eat(&mut self) {
+        self.left.status.lock().unwrap();
+        self.right.status.lock().unwrap();
+        self.eat();
+        self.left.status.unlock();
+        self.right.status.unlock();
+        self.think();
+    }
+
+
+    // soltar palillos
+}
